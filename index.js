2da7e088988e7ab4a2b0292b5de2149b09af6067
zhy200308 <93241859+zhy200308@users.noreply.github.com>
2023-03-18 22:39:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -59,3 +59,15 @@ async function bootstrap() {
 }
 
 bootstrap();
+// 一个用户发什么消息，就反弹什么消息的消息回复功能
+router.post('/message/post', async ctx => {
+  const { ToUserName, FromUserName, Content, CreateTime } = ctx.request.body;
+
+  ctx.body = {
+    ToUserName: FromUserName,
+    FromUserName: ToUserName,
+    CreateTime: +new Date(),
+    MsgType: 'text',
+    Content: `反弹你发的消息：${Content}`,
+  };
+});
